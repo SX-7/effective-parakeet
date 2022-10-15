63e026da318afb2ee0988f97effba1850f413ae8
SX_7 <sn1.test.preria.2002@gmail.com>
2022-10-15 21:59:40 +0200

    Weekend1, moved to branch

---
+++ Weekend1.java
@@ -0,0 +1,22 @@
+import java.util.Calendar;
+
+public class Weekend1 {
+    void day() {
+        Calendar c = Calendar.getInstance();
+        System.out.print("Dzisiaj jest ");
+        switch (c.get(Calendar.DAY_OF_WEEK)) {
+            case Calendar.MONDAY:
+                System.out.println("Poniedziałek");
+            case Calendar.TUESDAY:
+                System.out.println("Wtorek");
+            case Calendar.WEDNESDAY:
+                System.out.println("Środa");
+            case Calendar.THURSDAY:
+                System.out.println("Czwartek");
+            case Calendar.FRIDAY:
+                System.out.println("Piątek");
+            default:
+                System.err.println("Weekend");
+        }
+    }
+}
