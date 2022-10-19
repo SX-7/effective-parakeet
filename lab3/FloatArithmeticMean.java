4aa59cf38914d70e287fbbc864a1ecd26817a18d
SX_7 <sn1.test.preria.2002@gmail.com>
2022-10-19 14:41:07 +0200

    Added FloatArithmeticMean

---
+++ lab3/FloatArithmeticMean.java
@@ -0,0 +1,22 @@
+package lab3;
+
+public class FloatArithmeticMean {
+    public static void main(String[] args) {
+        if (args.length == 0)
+        {
+            System.out.println("Brak argumentów programu.");
+        }
+        else
+        {
+            float a = 0;
+            for (int i = 0; i < args.length; i++) {
+                System.out.printf("%10.3f%n",Float.parseFloat(args[i]));             
+                a=a+Float.parseFloat(args[i]);
+            }
+            System.out.println("----------");
+            System.out.printf("%10.3f%n",a);
+            float res=a/args.length;
+            System.out.printf("Średnia arytmetyczna: %.4f%n",res);
+        }
+    }
+}
