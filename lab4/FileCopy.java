8e27ce70d3521fbd62cbcf01842878bab303ece3
SX_7 <sn1.test.preria.2002@gmail.com>
2022-11-09 22:22:15 +0100

    Made workable FileCopy

--- lab4/FileCopy.java
+++ lab4/FileCopy.java
@@ -1,9 +1,10 @@
 package lab4;
 
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 
 public class FileCopy {
@@ -16,17 +17,27 @@ public class FileCopy {
         }
         // the operation
         try {
-            if (!Files.exists(Path.of(args[0]))) {
-                throw new FileNotFoundException("Plik " + args[0] + " nie istnieje.");
+            if (Files.isDirectory(Path.of(args[1]))) {
+                args[1] = args[1] + args[0].split(File.separator, 0)[args[0].split(File.separator, 0).length - 1];
             }
-            if (!Files.isRegularFile(Path.of(args[0]))) {
-                throw new FileNotFoundException(args[0] + " nie jest plikiem.");
+            if (Files.isDirectory(Path.of(args[0]))) {
+                System.err.println(args[0]+" jest folderem");
+                throw new Exception(args[0]+" jest folderem");
+            }
+            if (Files.exists(Path.of(args[1]))) {
+                System.err.println(args[1]+" już istnieje");
+                throw new Exception(args[1]+" już istnieje");
             }
             Files.copy(Path.of(args[0]), Path.of(args[1]));
-        } catch (FileAlreadyExistsException e) {
-            System.err.println("Plik "+args[1]+" już istnieje.");
+        } catch (NoSuchFileException e) {
+            System.err.println("Plik " + args[0] + " nie istnieje.");
+        } catch (AccessDeniedException e) {
+            System.err.println("Brak uprawnień dostępu");
         } catch (IOException e) {
+            System.err.println("Inny błąd wejścia");
             e.printStackTrace();
+        } catch (Exception e){
+            
         }
     }
 }
