919b29583a25dee481acd0919d077ed73c6d7e9a
SX_7 <sn1.test.preria.2002@gmail.com>
2022-11-11 14:33:30 +0100

    Removed unnecesary import

--- lab5/MultiplicationTable.java
+++ lab5/MultiplicationTable.java
@@ -3,7 +3,6 @@ package lab5;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
